1bc60981725e37284355784a1b14939fea2f55b6
Achraf.jhidri <jhidri1u@etu.univ-lorraine.fr>
2020-12-10 14:47:05 +0100

    shared Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ActiveDirective } from './shared/directives/active.directive';
 import { IngredientListComponent } from './panier/ingredient-list/ingredient-list.component';
 import { PanierComponent } from './panier/panier.component';
 import { AppRouting } from './app.routing'
 import { PanierService } from './shared/services/panier.service';
 import {CocktailModule} from './cocktail-container/cocktail.module'
+import { ShareModule } from './shared/modules/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,14 +17,14 @@ import {CocktailModule} from './cocktail-container/cocktail.module'
     ActiveDirective,
     PanierComponent,
     IngredientListComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AppRouting,
-    CocktailModule
+    CocktailModule,
+    ShareModule
   ],
   providers: [PanierService],
   bootstrap: [AppComponent]
